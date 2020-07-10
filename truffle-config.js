a2cd1cd5e49681b1de77b54453b935cffeba16ef
ArturVargas <arturvargas9226@gmail.com>
2020-07-10 07:58:16 -0500

    add networks config and upgrade web3 package

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,26 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    },
+    // ropsten: {
+    //   provider: () => new HDWalletProvider(process.env.MNENOMIC, `https://ropsten.infura.io/v3/${process.env.Project_Id}`),
+    //   network_id: 3,
+    //   gas: 3000000,
+    //   gasPrice: 10000000000
+    // },
+    // kovan: {
+    //   propvider: () => new HDWalletProvider(process.env.MNENOMIC, `https://kovan.infura.io/v3/${process.env.Project_Id}`),
+    //   network_id: 42,
+    //   gas: 3000000,
+    //   gasPrice: 10000000000
+    // }
   }
 };
